0247401cd9069ab7dea61df3e97e82519aca5ad8
kkawahara <k.kawahara@komei.jp>
2018-08-06 18:01:08 +0900

    ズーム設定

--- map_sample.js
+++ map_sample.js
@@ -1,4 +1,4 @@
-        var map = L.map('map').setView([35.883900, 139.626195], 26);
+        var map = L.map('map').setView([35.883900, 139.626195], 10);
 
         mapLink = '<a href="https://openstreetmap.org">OpenStreetMap</a>';
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
